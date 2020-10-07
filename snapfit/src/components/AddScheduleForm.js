494a8b9e6439c1126072a5795dd061f1a42f1c68
CYLee <cylee820621@gmail.com>
2020-10-07 12:54:43 -0400

    update schedule form

--- snapfit/src/components/AddScheduleForm.js
+++ snapfit/src/components/AddScheduleForm.js
@@ -4,44 +4,91 @@ import "../styles/addscheduleform.css";
 
 function AddScheduleForm(props) {
   const setOpen = props.setState;
-  const upperBody = ["Chest", "Arms", "Back", "Abdominal"];
-  const lowerBody = ["Quads", "Hamstrings", "Glutes", "Calves"];
-  const [bodyPart, setBodyPart] = useState({ value: "upper" });
 
-  function handleChange(e) {
-    setBodyPart({ value: e.target.value });
-  }
+  const secletion = [
+    {
+      target: "Select",
+      bodypart: [
+        { part: "Chest", exercise: ["Bench Press", "Low-incline Press", "Incline Press", "Dips", "Cable Fly"] },
+        { part: "Arms", exercise: ["Barbell Curl", "Hammer Curl", "Chin-Up", "Triceps Pushdown", "Lying Triceps Press", "Overhead Triceps Press"] },
+        { part: "Back", exercise: ["Barbell Deadlift", "Bent-over Row", "Pull-Up", "T-Bar Row", "Dumbell Row", "Cable Row"] },
+        { part: "Abdominal", exercise: ["Reverse Crunch", "Sitting Twists", "Dumbbell Side Bend", "Plank", "Side Plank"] }
+      ]
+    },
+    {
+      target: "Upper",
+      bodypart: [
+        { part: "Chest", exercise: ["Bench Press", "Low-incline Press", "Incline Press", "Dips", "Cable Fly"] },
+        { part: "Arms", exercise: ["Barbell Curl", "Hammer Curl", "Chin-Up", "Triceps Pushdown", "Lying Triceps Press", "Overhead Triceps Press"] },
+        { part: "Back", exercise: ["Barbell Deadlift", "Bent-over Row", "Pull-Up", "T-Bar Row", "Dumbell Row", "Cable Row"] },
+        { part: "Abdominal", exercise: ["Reverse Crunch", "Sitting Twists", "Dumbbell Side Bend", "Plank", "Side Plank"] }
+      ]
+    },
+    {
+      target: "Lower",
+      bodypart: [
+        { part: "Quads", exercise: ["Front Squat", "Bulgarian split squat", "Leg press", "Leg extension", "Squat"] },
+        { part: "Hamstrings", exercise: ["Glute Bridge", "Bench Hip Thrust", "Cable Pull-through", "Kettlebell Deadlift", "Romanian Deadlift", "Overhead Triceps Press"] },
+        { part: "Glutes", exercise: ["Walking Lunges", "Banded Lateral Squat", "Glute Bridge", "Weighted Goodmorning", "Banded Leg Lift", "Isometric Lunge"] },
+        { part: "Calves", exercise: ["Standing Calf Raise", "Seated calf raise", "Farmer’s walk on toes", "Jump rope"] }
+      ]
+    },
+    {
+      target: "Cardio",
+      exercises: ["Elliptical", "Running", "Cycling", "Swimming", "Sprinting", "Stair Climber", "Jumping Rope", "Kettlebells"]
+    }
+  ];
+
+  const [bodyPart, setBodyPart] = useState({ value: "" });
+  const [targetPart, setTargetBodyPart] = useState({ value: "" });
+  const [exercise, setExercise] = useState({ value: "" });
+
   function handleSubmit(e) {
-    alert("Form has been submitted!");
     e.preventDefault();
-    console.log(bodyPart.value);
+    console.log("bodyPart=" + bodyPart.value);
+    console.log("targetPart=" + targetPart.value);
+    console.log("exercise=" + exercise.value);
   }
+
   return (
     <div>
       <form className="schedule-form">
         <div className="form-group">
-          <select value={bodyPart.value} onChange={handleChange} className="form-control form-control-sm">
-            <option value="upper">Upper body</option>
-            <option value="lower">Lower body</option>
+          <select value={bodyPart.value} onChange={(e) => setBodyPart({ value: e.target.value })} className="form-control form-control-sm">
+            {secletion.map((part) => {
+              return <option value={part.target}>{part.target}</option>;
+            })}
           </select>
-          <select className="form-control form-control-sm">
-            {bodyPart.value == "upper"
-              ? upperBody.map((part) => {
-                  return <option>{part}</option>;
-                })
-              : lowerBody.map((part) => {
-                  return <option>{part}</option>;
-                })}
-          </select>
-          <select className="form-control form-control-sm">
-            <option>exercise</option>
-            <option>exercise</option>
-          </select>
-          <select className="form-control form-control-sm">
-            <option>exercise</option>
-            <option>exercise</option>
-          </select>
+
+          {bodyPart.value == "Upper" ? (
+            <select value={targetPart.value} onChange={(e) => setTargetBodyPart({ value: e.target.value })} className="form-control form-control-sm">
+              {secletion[1].bodypart.map((upperPart) => {
+                return <option value={upperPart.part}>{upperPart.part}</option>;
+              })}
+            </select>
+          ) : (
+            false
+          )}
+          {bodyPart.value == "Lower" ? (
+            <select value={targetPart.value} onChange={(e) => setTargetBodyPart({ value: e.target.value })} className="form-control form-control-sm">
+              {secletion[2].bodypart.map((lowerPart) => {
+                return <option value={lowerPart.part}>{lowerPart.part}</option>;
+              })}
+            </select>
+          ) : (
+            false
+          )}
+          {bodyPart.value == "Cardio" ? (
+            <select value={targetPart.value} onChange={(e) => setTargetBodyPart({ value: e.target.value })} className="form-control form-control-sm">
+              {secletion[3].exercises.map((exercise) => {
+                return <option value={exercise}>{exercise}</option>;
+              })}
+            </select>
+          ) : (
+            false
+          )}
         </div>
+
         <div className="d-flex btn-form">
           <Button type="submit" onClick={handleSubmit} variant="success" size="sm" block>
             Submit
