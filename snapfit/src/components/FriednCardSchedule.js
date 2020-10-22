7a6aa6fb337c3e64650a366d481ae7adecd4b8e3
CYLee <cylee820621@gmail.com>
2020-10-22 16:53:14 -0400

    update friendScheduleCard

--- snapfit/src/components/FriednCardSchedule.js
+++ snapfit/src/components/FriednCardSchedule.js
@@ -2,12 +2,22 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 function FriendCardSchedule(props) {
+  function handlePrev() {
+    alert("Prev clicked");
+  }
+  function handleNext() {
+    alert("Next clicked");
+  }
   return (
     <div className="p-2">
       <div className="d-flex justify-content-center">ScheduleContent</div>
       <div className="d-flex justify-content-around p-2">
-        <Button size="sm">prev</Button>
-        <Button size="sm">next</Button>
+        <Button onClick={handlePrev} size="sm">
+          prev
+        </Button>
+        <Button onClick={handleNext} size="sm">
+          next
+        </Button>
       </div>
     </div>
   );
