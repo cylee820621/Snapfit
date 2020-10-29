7bb6162a0f53cbd9719245a3801ff89a63048a86
CYLee <cylee820621@gmail.com>
2020-10-29 13:33:02 -0400

    update

--- snapfit/src/components/Chatroom/ChatRoom.js
+++ snapfit/src/components/Chatroom/ChatRoom.js
@@ -1,11 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import "./chatroom.css";
 
 function ChatRoom() {
+  const [chatroom, setChatroom] = useState(false);
+
   return (
     <div className="chat-room px-1">
-      <Button size="lg">Message</Button>
+      <Button onClick={() => setChatroom(!chatroom)} size="lg">
+        Messenger
+      </Button>
+      {chatroom && (
+        <div className="chatroom-box shadow-lg">
+          <div className="d-flex pl-4 pt-3">
+            <h4 className="font-color-black ">Messenger</h4>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
