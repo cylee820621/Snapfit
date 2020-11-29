081a49368146f6ddd10e12075dc2277fbfee20a8
CYLee <cylee820621@gmail.com>
2020-11-28 20:29:56 -0500

    update

--- snapfit/src/components/HealthInfo.js
+++ snapfit/src/components/HealthInfo.js
@@ -7,52 +7,69 @@ function HealthInfo() {
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
   const [age, setAge] = useState("");
-  const [gender, setGender] = useState("");
+  const [gender, setGender] = useState("male");
   const [level, setLevel] = useState("1");
   const [BMI, setBMI] = useState("");
   const [BMR, setBMR] = useState("");
 
-  const getbmrbmi = async () => {
-    setLoading(true);
-    const options1 = {
-      method: "GET",
-      url: `https://urvipaithankar.herokuapp.com/bmr/index.php/${height}/${weight}/${age}/${gender}`,
-      headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS" }
-    };
-    const options2 = {
-      method: "GET",
-      url: `https://urvipaithankar.herokuapp.com/bmi/index.php/${height}/${weight}/${age}/${gender}`,
-      headers: { crossDomain: true, "Access-Control-Allow-Origin": "*" }
-    };
-    const resbmr = await Axios.request(options1);
-    const resbmi = await Axios.request(options2);
-    if (resbmr && resbmi) {
-      console.log(resbmr);
-      console.log(resbmi);
-      setLoading(false);
+  function handleCalculate() {
+    setBMI(weight / (0.01 * height * 0.01 * height));
+    if (gender === "female") {
+      if (level === "1") {
+        setBMR(1.2 * (655 + 9.6 * weight + 1.8 * height - 4.7 * age));
+      }
+      if (level === "2") {
+        setBMR(1.375 * (655 + 9.6 * weight + 1.8 * height - 4.7 * age));
+      }
+      if (level === "3") {
+        setBMR(1.55 * (655 + 9.6 * weight + 1.8 * height - 4.7 * age));
+      }
+      if (level === "4") {
+        setBMR(1.725 * (655 + 9.6 * weight + 1.8 * height - 4.7 * age));
+      }
+      if (level === "5") {
+        setBMR(1.9 * (655 + 9.6 * weight + 1.8 * height - 4.7 * age));
+      }
+    } else {
+      console.log("male");
+      if (level === "1") {
+        setBMR(1.2 * (66 + 13.7 * weight + 5 * height - 6.8 * age));
+      }
+      if (level === "2") {
+        setBMR(1.375 * (66 + 13.7 * weight + 5 * height - 6.8 * age));
+      }
+      if (level === "3") {
+        setBMR(1.55 * (66 + 13.7 * weight + 5 * height - 6.8 * age));
+      }
+      if (level === "4") {
+        setBMR(1.725 * (66 + 13.7 * weight + 5 * height - 6.8 * age));
+      }
+      if (level === "5") {
+        setBMR(1.9 * (66 + 13.7 * weight + 5 * height - 6.8 * age));
+      }
     }
-  };
+  }
 
   return (
     <Container fluid className="justify-content-center">
-      <Container className="d-flex">
-        <div className="pr-1">Weight :</div> <input placeholder="kilogram" as="text" value={weight} onChange={(e) => setWeight(e.target.value)} />
+      <Container className="d-flex my-2 ">
+        <div className="pr-1 h5">Weight :</div> <input placeholder="kilogram" as="text" value={weight} onChange={(e) => setWeight(e.target.value)} />
       </Container>
-      <Container className="d-flex mt-1">
-        <div className="pr-1">Height :</div> <input placeholder="cm" as="text" value={height} onChange={(e) => setHeight(e.target.value)} />
+      <Container className="d-flex my-2">
+        <div className="pr-1 h5">Height :</div> <input placeholder="cm" as="text" value={height} onChange={(e) => setHeight(e.target.value)} />
       </Container>
-      <Container fluid className="d-flex mt-1">
-        <div className="pr-1">Age :</div> <input as="text" value={age} onChange={(e) => setAge(e.target.value)} />
+      <Container fluid className="d-flex my-2">
+        <div className="pr-1 h5">Age :</div> <input placeholder="years" as="text" value={age} onChange={(e) => setAge(e.target.value)} />
       </Container>
-      <Container fluid className="d-flex mt-1">
-        <div className="pr-1">Gender :</div>
+      <Container fluid className="d-flex my-2">
+        <div className="pr-1 h5">Gender :</div>
         <select onChange={(e) => setGender(e.target.value)}>
           <option value="">Male</option>
-          <option value="femail">Female</option>
+          <option value="female">Female</option>
         </select>
       </Container>
-      <Container fluid className="d-flex  mt-1">
-        <div className="pr-1">Exercise Level :</div>
+      <Container fluid className="d-flex my-2 ">
+        <div className="pr-1 h5">Exercise Level :</div>
         <select onChange={(e) => setLevel(e.target.value)}>
           <option value="1">little or no exercise</option>
           <option value="2">light exercise/sports 1-3 days/week</option>
@@ -67,19 +84,19 @@ function HealthInfo() {
             <Spinner animation="border" />
           </div>
         ) : (
-          <Button
-            onClick={() => {
-              getbmrbmi();
-            }}
-          >
+          <Button block size="sm" onClick={handleCalculate}>
             Calulate
           </Button>
         )}
       </Container>
 
       <Container fluid className="d-flex justify-content-center mt-1">
-        <div>BMI:</div>
-        <div>{}</div>
+        <div className="pr-1 h4">BMI:</div>
+        <div>{BMI}</div>
+      </Container>
+      <Container fluid className="d-flex justify-content-center mt-1">
+        <div className="pr-1 h4">BMR:</div>
+        <div>{BMR}</div>
       </Container>
       <Container fluid className="d-flex justify-content-center mt-1">
         Food calorie calculator
