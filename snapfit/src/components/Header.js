e1f295a55cee2d6ea94ee9e8077c58e227ce0f68
CYLee <cylee820621@gmail.com>
2020-12-01 14:56:19 -0500

    update logo

--- snapfit/src/components/Header.js
+++ snapfit/src/components/Header.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Image } from "react-bootstrap";
 import HeaderNewFriend from "./HeaaderNewFriend";
 import Messenger from "./Messenger";
 import "../styles/header.css";
+import logo from "../assets/logo.png";
 
 function HeaderLoggedIn() {
   return (
     <nav className="navbar navbar-expand-md shadow-sm mb-3" id="mainNav">
       <Container fluid className="d-flex justify-content-between">
-        <Link id="logo" className="navbar-brand" to="/">
-          SNAPFIT
-        </Link>
+        <div className="d-flex ">
+          <Image className="logo-btm mt-1" src={logo} />
+          <Link id="logo" className="navbar-brand justify-content-center align-centent-center" to="/">
+            SNAPFIT
+          </Link>
+        </div>
+
         <div className="d-flex justify-content-between align-items-center px-2">
           <div className="d-flex justify-content-between align-items-center">
             <HeaderNewFriend />
